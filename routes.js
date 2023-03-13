26111850514ef55253b5ad96d9385d2ef6d4cd36
heroyudo <hero@pens.ac.id>
2023-03-14 05:10:08 +0700

    homepage update 3

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ const routes = [
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-            return 'Homepage update 2';
+            return 'Homepage update 3';
         },
     },
 ];
