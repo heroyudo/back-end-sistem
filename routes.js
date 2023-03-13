5433567c0b51898d96e6d2055cce1b4137f88b67
heroyudo <hero@pens.ac.id>
2023-03-13 14:10:51 +0700

    update 2

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const routes = [
         method: '*',
         path: '/',
         handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut - update 1';
+            return 'Halaman tidak dapat diakses dengan method tersebut - update 2';
         },
     },
     {
